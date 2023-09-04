2505033f7ea7bf5242394198b15c0b3d133f542f
Siddharth Gupta <34214713+sidd462@users.noreply.github.com>
2023-09-04 23:10:43 +0530

    Update inference.py

--- Inventory Replenishment System/Random forest/inference.py
+++ Inventory Replenishment System/Random forest/inference.py
@@ -4,6 +4,7 @@ import click
 import numpy as np
 import random
 import warnings
+import holidays
 
 # Set seeds for reproducibility
 np.random.seed(42)
@@ -12,11 +13,34 @@ random.seed(42)
 # Ignore warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+def add_holiday_weekday_columns(data):
+    """
+    Adds 'IsHoliday' and 'Weekday' columns to the provided data.
+    
+    Parameters:
+    - data (pd.DataFrame): DataFrame containing a 'Date' column.
+
+    Returns:
+    - data (pd.DataFrame): DataFrame with added 'IsHoliday' and 'Weekday' columns.
+    """
+    # Define a simple list of US holidays (month, day)
+    
+    # Convert 'Date' column to datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    
+    # Add 'IsHoliday' column: 1 if the date is a holiday, 0 otherwise
+    data['IsHoliday'] = data['Date'].apply(lambda x: x in holidays.US()).astype(int)
+    
+    # Add 'Weekday' column: 0 (Monday) to 6 (Sunday)
+    data['Weekday'] = data['Date'].dt.weekday
+    
+    return data
+
 @click.command()
 @click.argument('input_csv', type=str)
 @click.option('--start_date', 'S', type=str, required=True, help='Start date for replenishment calculation')
 @click.option('--duration', 'D', type=int, required=True, help='Duration for which replenishment is to be calculated')
-def main(input_csv, S, D):
+def main(input_csv,S, D):
     """
     Calculates the recommended restock amount for a given duration, starting from a specified date, 
     using a trained forecasting model.
@@ -34,15 +58,19 @@ def main(input_csv, S, D):
 
     # Load the CSV file to get the last day's inventory
     data = pd.read_csv(input_csv)
-    last_inventory = data['Inventory Level'].iloc[-1]
+    last_inventory = data['Inventory'].iloc[-1]
     
     # Prepare data for the specified range of dates (simplified for this example)
     dates = pd.date_range(start=S, periods=D)
     future_data = pd.DataFrame({'Date': dates})
+    # Add 'IsHoliday' and 'Weekday' columns
+    future_data = add_holiday_weekday_columns(future_data)
     
+    # Drop the 'Date' column (since it's not used in the model) 
+    # and prepare the input for prediction
+    X_forecast = future_data.drop(columns=['Date'])
     # Predict sales for the range
-    forecasted_sales = model.predict(future_data)
-    
+    forecasted_sales = model.predict(X_forecast)
     # Calculate total forecasted sales and determine restock amount
     total_forecasted_sales = forecasted_sales.sum()
     restock_amount = max(0, total_forecasted_sales - last_inventory)
