b0cd2d3f77a8fcd30481db6b470eee14df51507a
Siddharth Gupta <34214713+sidd462@users.noreply.github.com>
2023-09-05 02:45:05 +0530

    Update inference.py

--- Inventory Management/Random Forest/inference.py
+++ Inventory Management/Random Forest/inference.py
@@ -4,6 +4,7 @@ import click
 import numpy as np
 import random
 import warnings
+import holidays
 
 # Set seeds for reproducibility
 np.random.seed(42)
@@ -11,6 +12,24 @@ random.seed(42)
 
 # Ignore warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
+def add_holiday_weekday_columns(data):
+    """
+    Adds 'IsHoliday' column to the provided data.
+    
+    Parameters:
+    - data (pd.DataFrame): DataFrame containing a 'Date' column.
+
+    Returns:
+    - data (pd.DataFrame): DataFrame with added 'IsHoliday' column.
+    """
+    # Convert 'Date' column to datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    
+    # Add 'IsHoliday' column: 1 if the date is a holiday, 0 otherwise
+    data['IsHoliday'] = data['Date'].apply(lambda x: x in holidays.US()).astype(int)
+        
+    return data
 
 @click.command()
 @click.option('--end_date', prompt='Enter the end date for prediction (MM-DD-YYYY)', help='The end date for which inventory should be predicted.')
@@ -31,13 +50,13 @@ def predict_inventory(end_date):
     end_date_formatted = pd.to_datetime(end_date, format='%m-%d-%Y')
 
     # Predict the inventory till the specified end date
-    data = pd.read_csv('/path')
+    data = pd.read_csv('inventory forcast/random forest/scripts/dataset.csv')
     last_date = pd.to_datetime(data['Date'].max())
     future_dates = pd.date_range(start=last_date + pd.Timedelta(days=1), end=end_date_formatted)
     future_data = pd.DataFrame({
-        'Date': future_dates,
-        'Sale Count': data['Sale Count'].mean()  # Using average sale count as placeholder
+        'Date': future_dates
     })
+    future_data = add_holiday_weekday_columns(future_data)
     future_data['Day_of_Week'] = future_data['Date'].dt.dayofweek
     future_data['Month'] = future_data['Date'].dt.month
     future_data['Year'] = future_data['Date'].dt.year
